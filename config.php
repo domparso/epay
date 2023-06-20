80f00367a99ea01dcdbe5aa9d90a1d9afb9fc0e3
domparso <domparso@hotmail.com>
2023-06-20 14:21:53 +0800

    first commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+/*数据库配置*/
+$dbconfig=array(
+	'host' => 'localhost', //数据库服务器
+	'port' => 3306, //数据库端口
+	'user' => '97hanmanpay', //数据库用户名
+	'pwd' => 'ibt3fZ2HPEnXcGtW', //数据库密码
+	'dbname' => '97hanmanpay', //数据库名
+	'dbqz' => 'pay' //数据表前缀
+);
